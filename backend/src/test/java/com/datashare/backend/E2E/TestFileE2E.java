d2c385a30af39fa7e00998b55cb4e61e022e420e
edourde FERRARI <abdirrazzak44@gmail.co>
2026-04-12 17:09:36 +0200

    Add test E2E testDonwloadTokenInvalid

--- backend/src/test/java/com/datashare/backend/E2E/TestFileE2E.java
+++ backend/src/test/java/com/datashare/backend/E2E/TestFileE2E.java
@@ -14,6 +14,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
 @AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class TestFileE2E {
@@ -22,53 +23,63 @@ public class TestFileE2E {
 	private MockMvc mockMvc;
 	@Autowired
 	private ObjectMapper mapper;
-	
-	
 
 	@Test
 	void fullDownloadClien() throws Exception {
+		String loginRequest = """
+				{
+				"login":"mario@gmail.com",
+				"password":"pass123"
+				}
+				""";
 		SignupRequest signup = new SignupRequest();
-		signup.setLogin("mario");
+		signup.setLogin("mario@gmail.com");
 		signup.setPassword("pass123");
 		mockMvc.perform(post("/api/register").contentType(MediaType.APPLICATION_JSON)
 				.content(mapper.writeValueAsString(signup))).andExpect(status().isOk());
 
-		String LoginResponse = mockMvc.perform(post("/api/login").contentType(MediaType.APPLICATION_JSON).content("""
-				{
-				"login":"mario",
-				"password":"pass123"
-				}
-				""")).andExpect(status().isOk()).andReturn().getResponse().getContentAsString();
+		String LoginResponse = mockMvc
+				.perform(post("/api/login").contentType(MediaType.APPLICATION_JSON).content(loginRequest))
+				.andExpect(status().isOk()).andReturn().getResponse().getContentAsString();
 		String token = mapper.readTree(LoginResponse).get("token").asText();
-		
-		MockMultipartFile file = new MockMultipartFile(
-                "file",
-                "test.txt",
-                "text/plain",
-                "informationnnnn".getBytes()
-        );
-		String UploadResponse=mockMvc.perform(multipart("/api/files/upload")
-				.file(file)
-				.param("ExpireDays", "3")
-				.header("Authorization","Bearer "+token))
-				.andExpect(status().isOk())
-				.andExpect(jsonPath("$.downloadToken").exists())
-				.andReturn()
-				.getResponse()
+
+		MockMultipartFile file = new MockMultipartFile("file", "test.txt", "text/plain", "informationnnnn".getBytes());
+		String UploadResponse = mockMvc
+				.perform(multipart("/api/files/upload").file(file).param("expireDays", "3").header("Authorization",
+						"Bearer " + token))
+				.andExpect(status().isOk()).andExpect(jsonPath("$.downloadToken").exists()).andReturn().getResponse()
 				.getContentAsString();
-		
-		String donloadToken= mapper
-				.readTree(UploadResponse)
-				.get("downloadToken")
-				.asText();
-		
-		 mockMvc.perform(get("/api/files/download/" + donloadToken))
-         .andExpect(status().isOk())
-         .andExpect(header().string("Content-Disposition",
-                 org.hamcrest.Matchers.containsString("test.txt")))
-         .andExpect(content().string("informationnnnn"));
-		
-				
-				
+
+		String donloadToken = mapper.readTree(UploadResponse).get("downloadToken").asText();
+
+		mockMvc.perform(get("/api/files/download/" + donloadToken)).andExpect(status().isOk())
+				.andExpect(header().string("Content-Disposition", org.hamcrest.Matchers.containsString("test.txt")))
+				.andExpect(content().string("informationnnnn"));
 	}
+
+	@Test
+	void testLoginFail() throws Exception {
+		SignupRequest signup = new SignupRequest();
+		signup.setLogin("Test@gmail.com");
+		signup.setPassword("pass123");
+		mockMvc.perform(post("/api/register").contentType(MediaType.APPLICATION_JSON)
+				.content(mapper.writeValueAsString(signup))).andExpect(status().isOk());
+
+		String badRequest = """
+				{
+				"login":"badLogin",
+				"password":"badPassword"
+				}
+				""";
+		mockMvc.perform(post("/api/login").contentType(MediaType.APPLICATION_JSON).content(badRequest))
+				.andExpect(status().isUnauthorized());
+	}
+	@Test
+	void testDonwloadTokenInvalid()throws Exception {
+		String invalidToken="Token-incorrect";
+		mockMvc.perform(get("/api/files/download/"+invalidToken))
+		.andExpect(status().isNotFound());
+	}
+	
+	
 }
